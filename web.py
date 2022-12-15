92bbec61eb235d5b5e7192d62a625ac05c9c770c
Canthar90 <d.cieslak90@gmail.com>
2022-12-15 10:58:17 +0100

    Added working item addition logic

--- web.py
+++ web.py
@@ -4,6 +4,17 @@ import managment
 
 todos = managment.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    managment.save_to_txt(todos)
+    
+    
+# def generate_todos():
+#     for todo in todos:
+#         st.checkbox(todo)
+
+
 st.title("Tasks list App")
 st.subheader("This im my todo/tasks app")
 st.write("This app is to increase your productivity.")
@@ -12,4 +23,7 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="", placeholder="Input new todo....")
+st.text_input(label=" ", placeholder="Input new todo....",
+              on_change=add_todo, key="new_todo")
+
+
