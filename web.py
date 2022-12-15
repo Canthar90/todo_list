dad388067cee45e1dcb96398d05b488d8bab8440
Canthar90 <d.cieslak90@gmail.com>
2022-12-15 13:18:57 +0100

    Added checking tasks out mechanics

--- web.py
+++ web.py
@@ -2,28 +2,36 @@ import streamlit as st
 import managment
 
 
+
 todos = managment.get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
-    todos.append(todo)
-    managment.save_to_txt(todos)
+    if todo.strip():
+        todos.append(todo)
+        managment.save_to_txt(todos)
     
-    
-# def generate_todos():
-#     for todo in todos:
-#         st.checkbox(todo)
-
 
 st.title("Tasks list App")
 st.subheader("This im my todo/tasks app")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+checkboxes = []
+for index, todo in enumerate(todos):
+    # st.checkbox(todo, key=todo)
+    checkboxes.append(index)
+    checkbox = st.checkbox(todo, key=index )
+    if checkbox:
+        todos.pop(index)
+        managment.save_to_txt(todos)
+        del st.session_state[index]
+        st.experimental_rerun()
+    
 
 
 st.text_input(label=" ", placeholder="Input new todo....",
               on_change=add_todo, key="new_todo")
 
 
+
+st.session_state
